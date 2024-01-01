36a41ff97a97d6e25338664438f10fd8c897bc16
gapirajat <rajat.shinde201@pccoepune.org>
2024-01-01 18:33:27 +0000

    Initital commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = require("./models");
+
+const userRouter = require("./routes/User");
+app.use("/auth", userRouter);
+
+// async function main() {
+//   await db.sequelize.sync({force:true})
+// }
+
+// main()
+
+db.sequelize.sync().then((result) => {
+    app.listen(3001, () => {
+      console.log("Server running on port 3001");
+    });
+  });
