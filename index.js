7a17b54bd2d9acffcf0c20ef48ff706da9a21dfb
gapirajat <rajat.shinde201@pccoepune.org>
2024-01-02 10:43:59 +0000

    cors

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://jobwork-portal.vercel.app/"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 const db = require("./models");
 
 const userRouter = require("./routes/User");
